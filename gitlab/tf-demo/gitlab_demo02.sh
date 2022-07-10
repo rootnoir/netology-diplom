2c7a7da51a2fc8b1cada67bc3c945297646ce8f9
noir <noirdb@yandex.ru>
2022-07-10 16:23:55 +0300

    up

--- gitlab/tf-demo/gitlab_demo02.sh
+++ gitlab/tf-demo/gitlab_demo02.sh
@@ -11,7 +11,7 @@ echo "==========================================================================
 git --git-dir=../gitlab/wp-project/.git --work-tree=../gitlab/wp-project add .
 git --git-dir=../gitlab/wp-project/.git --work-tree=../gitlab/wp-project commit -m 'update wordpress'
 export GIT_SSL_NO_VERIFY=1
-git --git-dir=../gitlab/wp-project/.git --work-tree=../gitlab/wp-project tag -a v1.1 -m 'version 1.0'
+git --git-dir=../gitlab/wp-project/.git --work-tree=../gitlab/wp-project tag -a v1.0 -m 'version 1.0'
 git --git-dir=../gitlab/wp-project/.git --work-tree=../gitlab/wp-project push
 git --git-dir=../gitlab/wp-project/.git --work-tree=../gitlab/wp-project push --tags
 echo "============================================================================="
